ddd5470edb30165794aad0bbca55944fa59cebbb
kyleyiyang <kyleyiyang@gmail.com>
2020-10-28 23:06:58 -0400

    Update trimBST.java

--- trimBST.java
+++ trimBST.java
@@ -20,37 +20,50 @@ class Solution {
             return searchTree(root,low);
         } else {
             temp = newTree(root,low,high);
-            
+           //System.out.println("temp="+temp.val+"temp.right="+temp.right.val);
             return cleanTree(temp,low,high);
         }
     }
     public static TreeNode newTree(TreeNode r,int l, int h) {
         if (r!=null) {
             if (r.val<l) {
-                System.out.println("before="+r.val);
+                TreeNode sm = r.right;
                 r.left = null;
                 r=r.right;
-               // System.out.println("after="+r.val);
-                newTree(r,l,h);
+                newTree(sm,l,h);
             } else if (r.val>h) {
+                TreeNode lg = r.left;
                 r.right = null;
                 r=r.left;
-                newTree(r,l,h);
+                newTree(lg,l,h);
             } else {
-                if (r.val==l || (r.val==h && r.left.val<l)) {
-                    r.left=null;
-                    r.right=null;
-                } else if (r.left!=null) {
+                //if (r.val==l || (r.val==h && r.left.val<l)) {
+                    //System.out.println(r.val);
+                  //  r.left=null;
+                  //  r.right=null;
+                //} else 
+                if (r.val==h) {
+                    r.right = null;
+                }
+                if (r.val == l) {
+                    r.left = null;
+                }
+                if (r.left!=null) {
+                    
                     if (r.left.val<l && r.left.right!=null) {
                         r.left=r.left.right;
                     }
-                    newTree(r.left,l,h);
-                } else if (r.right!=null) {
+                    //newTree(r.left,l,h);
+                } //else 
+                if (r.right!=null) {
                     if (r.right.val>h && r.right.left!=null) {
                         r.right = r.right.left;
                     }
-                    newTree(r.right,l,h);
+                    //newTree(r.right,l,h);
                 }
+                //System.out.println(r.val);
+                newTree(r.left,l,h);
+                newTree(r.right,l,h);
             }
         }
         return r;
@@ -60,7 +73,7 @@ class Solution {
         if (r!=null) {
             TreeNode left = r.left;
             TreeNode right = r.right;
-            System.out.println(r.val);
+            //System.out.println(r.val);
             if (r.val<l) {
                 r=right;
                 //System.out.println(r.val);
