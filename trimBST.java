d3bf93eb0a698df1bc05cf72a69b155b12528a3f
kyleyiyang <kyleyiyang@gmail.com>
2020-10-29 23:20:07 -0400

    Update trimBST.java

--- trimBST.java
+++ trimBST.java
@@ -20,7 +20,7 @@ class Solution {
             return searchTree(root,low);
         } else {
             temp = newTree(root,low,high);
-           //System.out.println("temp="+temp.val+"temp.right="+temp.right.val);
+           System.out.println("temp="+temp.val+";temp.right="+temp.right.val);
             return cleanTree(temp,low,high);
         }
     }
@@ -34,57 +34,68 @@ class Solution {
             } else if (r.val>h) {
                 TreeNode lg = r.left;
                 r.right = null;
-                r=r.left;
+                r=lg;
                 newTree(lg,l,h);
             } else {
-                //if (r.val==l || (r.val==h && r.left.val<l)) {
-                    //System.out.println(r.val);
-                  //  r.left=null;
-                  //  r.right=null;
-                //} else 
                 if (r.val==h) {
                     r.right = null;
+                    left_connect(r,l);
+                    /*if (r.left!=null && r.left.val<l && r.left.right!=null) {
+                        r.left=r.left.right;
+                    }*/
                 }
                 if (r.val == l) {
                     r.left = null;
+                    right_connect(r,h);
+                   /* if (r.right!=null && r.right.val>h && r.right.left!=null) {
+                        r.right = r.right.left;
+                    }*/
                 }
-                if (r.left!=null) {
+                left_connect(r,l);
+                right_connect(r,h);
+                /*if (r.left!=null) {
                     
                     if (r.left.val<l && r.left.right!=null) {
                         r.left=r.left.right;
                     }
-                    //newTree(r.left,l,h);
-                } //else 
+                }
                 if (r.right!=null) {
                     if (r.right.val>h && r.right.left!=null) {
                         r.right = r.right.left;
                     }
-                    //newTree(r.right,l,h);
-                }
-                //System.out.println(r.val);
+                }*/
                 newTree(r.left,l,h);
                 newTree(r.right,l,h);
             }
         }
         return r;
     }
-    
+    public static void left_connect(TreeNode r, int l) {
+        if (r.left!=null && r.left.val<l && r.left.right!=null) {
+            r.left=r.left.right;
+        }
+    }
+    public static void right_connect(TreeNode r, int h) {
+        if (r.right!=null && r.right.val>h && r.right.left!=null) {
+            r.right = r.right.left;
+        }
+    }
     public TreeNode cleanTree(TreeNode r, int l, int h) {
         if (r!=null) {
             TreeNode left = r.left;
             TreeNode right = r.right;
-            //System.out.println(r.val);
             if (r.val<l) {
+                System.out.println("1st r="+r.val);
                 r=right;
-                //System.out.println(r.val);
+                System.out.println("2nd r="+r.val);
             }
             if (r.val>h) {
                 r=left;
             }
             cleanTree(left,l,h);
             cleanTree(right,l,h);
-            
         }
+        
         return r;
     }
     
